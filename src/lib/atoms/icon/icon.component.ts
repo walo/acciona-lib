eb979da43a6b70da599c191cf7992e2595a60905
Walter Utria <walter.utria@gmail.com>
2026-03-10 16:49:35 -0500

    Add SVG icon sprite support

--- src/lib/atoms/icon/icon.component.ts
+++ src/lib/atoms/icon/icon.component.ts
@@ -11,9 +11,13 @@ export class AcIconComponent {
   @Input({ required: true }) name!: string;
   @Input() size = 16;
   @Input() spin = false;
+  @Input() spritePath = '/assets/acciona-ui-lib/icons.svg';
 
   get iconClass(): string {
-    const spinClass = this.spin ? ' ac-icon--spin' : '';
-    return `ac-icon ac-icon--${this.name}${spinClass}`;
+    return this.spin ? 'ac-icon ac-icon--spin' : 'ac-icon';
+  }
+
+  get iconHref(): string {
+    return ${this.spritePath}#;
   }
 }
